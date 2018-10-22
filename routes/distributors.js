91c499b4607e947e40334ebdc892c8758f9a9fa3
Héctor BlisS <hectorbliss@MacBook-Pro-de-Hector.local>
2018-10-21 23:46:38 -0500

    trying to make user

--- routes/distributors.js
+++ routes/distributors.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const Model = require('../models/Distributor')
+const User = require('../models/User')
 const {verifyToken} = require('../helpers/jwt')
-const fetch = require('fetch')
+const bcrypt                         = require('bcrypt')
+const mailer                         = require('../helpers/mailer')
+const QRCode                         = require('qrcode')
 
 router.get('/', (req,res, next)=>{
     Model.find()
@@ -13,14 +16,7 @@ router.get('/', (req,res, next)=>{
 router.post('/', (req,res, next)=>{
     Model.create(req.body)
     .then(item=>{
-        item.username = item.email
-        fetch('/signup', {
-            method:"post",
-            headers:{
-                "Content-Type":"application/json"
-            },
-            body:JSON.stringify(item)
-        })
+        createUser(item)
         res.status(201).json(item)
     })
     .catch(e=>next(e))
@@ -43,6 +39,28 @@ router.delete('/:id', (req,res, next)=>{
     .then(item=>res.status(200).json(item))
     .catch(e=>next(e))
 })
+
+function createUser(item){
+    const {email} = item
+    item.username = email
+    let h4$hP4$$ = bcrypt.hashSync(email, bcrypt.genSaltSync(7))
+    User.register(item, h4$hP4$$)
+    //User.register(req.body, req.body.password)
+    .then(user => {
+      const {_id} = user
+      link = `https://backendallende.herokuapp.com//profile/${_id}`
+      //link = `http://localhost:3001/profile/${_id}`
+      //link = `https://${req.headers.host}/profile/${_id}`
+      QRCode.toDataURL(link, (err, QR) => {
+        if (err) throw err
+        User.findByIdAndUpdate(_id, { QR }, { new: true })
+        .then(user => {
+          mailer.welcomeMail(user.business_name, user.email, h4$hP4$$)
+        })
+        .catch(err => console.log(err))
+      })
+    })
+}
 
 
 
